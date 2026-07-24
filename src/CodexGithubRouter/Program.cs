5646b7fce6dec8dac5cb43c3dd998fe495214db8
Çağatay Dilsiz <cagatay.dilsiz@aryazilim.com.tr>
2026-07-24 23:26:34 +0300

    Add initial project structure and main program logic

---
+++ src/CodexGithubRouter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+if (args.Length == 0)
+{
+    PrintHelp();
+    return 0;
+}
+
+return args[0].ToLowerInvariant() switch
+{
+    "--version" or "-v" => PrintVersion(),
+    "--help" or "-h" => PrintHelp(),    
+    _ => UnknownCommand(args[0])
+};
+
+static int PrintVersion()
+{
+    Console.WriteLine("0.0.1");
+    return 0;
+}
+
+static int PrintHelp()
+{ 
+
+    Console.WriteLine(
+        """
+        Codex Github Router
+
+        Usage:        
+          cgr --version
+          cgr --help
+        """);
+
+    return 0;
+}
+
+static int UnknownCommand(string command)
+{
+    Console.Error.WriteLine($"Bilinmeyen komut: {command}");
+    return 2;
+}
